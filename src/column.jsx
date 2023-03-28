f189524dbb0837c2e72380ac4bb0a36a6329d03f
Sebastian OM <sebastianobandomorales@gmail.com>
2023-03-28 06:37:28 -0500

    Reorder Columns with react-beutiful-dnd

--- src/column.jsx
+++ src/column.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Droppable } from 'react-beautiful-dnd';
-import Task from './task';
+import React from "react";
+import styled from "styled-components";
+import { Droppable, Draggable } from "react-beautiful-dnd";
+import Task from "./task";
 
 const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
+  background-color:white;
   border-radius: 2px;
   width: 220px;
 
@@ -18,7 +19,7 @@ const Title = styled.h3`
 const TaskList = styled.div`
   padding: 8px;
   transition: background-color 0.2s ease;
-  background-color: ${props => (props.isDraggingOver ? 'skyblue' : 'white')};
+  background-color: ${(props) => (props.isDraggingOver ? "skyblue" : "inherit")};
   flex-grow: 1;
   min-height: 100px;
 `;
@@ -26,26 +27,31 @@ const TaskList = styled.div`
 export default class Column extends React.Component {
   render() {
     return (
-      <Container>
-        <Title>{this.props.column.title}</Title>
-        <Droppable
-          droppableId={this.props.column.id}
-          isDropDisabled={this.props.isDropDisabled}
-        >
-          {(provided, snapshot) => (
-            <TaskList
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-              isDraggingOver={snapshot.isDraggingOver}
+      <Draggable draggableId={this.props.column.id} index={this.props.index}>
+        {(provided) => (
+          <Container {...provided.draggableProps} ref={provided.innerRef}>
+            <Title {...provided.dragHandleProps}>{this.props.column.title}</Title>
+            <Droppable
+              droppableId={this.props.column.id}
+              isDropDisabled={this.props.isDropDisabled}
+              type="task"
             >
-              {this.props.tasks.map((task, index) => (
-                <Task key={task.id} task={task} index={index} />
-              ))}
-              {provided.placeholder}
-            </TaskList>
-          )}
-        </Droppable>
-      </Container>
+              {(provided, snapshot) => (
+                <TaskList
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                  isDraggingOver={snapshot.isDraggingOver}
+                >
+                  {this.props.tasks.map((task, index) => (
+                    <Task key={task.id} task={task} index={index} />
+                  ))}
+                  {provided.placeholder}
+                </TaskList>
+              )}
+            </Droppable>
+          </Container>
+        )}
+      </Draggable>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '@atlaskit/css-reset';
-import styled from 'styled-components';
-import { DragDropContext } from 'react-beautiful-dnd';
-import initialData from './initial-data';
-import Column from './column';
+import React from "react";
+import ReactDOM from "react-dom";
+import "@atlaskit/css-reset";
+import styled from "styled-components";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import initialData from "./initial-data";
+import Column from "./column";
 
 const Container = styled.div`
   display: flex;
@@ -13,7 +13,7 @@ const Container = styled.div`
 class App extends React.Component {
   state = initialData;
 
-  onDragStart = start => {
+  onDragStart = (start) => {
     const homeIndex = this.state.columnOrder.indexOf(start.source.droppableId);
 
     this.setState({
@@ -21,12 +21,12 @@ class App extends React.Component {
     });
   };
 
-  onDragEnd = result => {
+  onDragEnd = (result) => {
     this.setState({
       homeIndex: null,
     });
 
-    const { destination, source, draggableId } = result;
+    const { destination, source, draggableId, type } = result;
 
     if (!destination) {
       return;
@@ -36,6 +36,19 @@ class App extends React.Component {
       destination.droppableId === source.droppableId &&
       destination.index === source.index
     ) {
+      return;
+    }
+
+    if (type === 'column') {
+      const newColumnOrder = Array.from(this.state.columnOrder);
+      newColumnOrder.splice(source.index, 1);
+      newColumnOrder.splice(destination.index, 0, draggableId);
+
+      const newState = {
+        ...this.state,
+        columnOrder: newColumnOrder,
+      };
+      this.setState(newState);
       return;
     }
 
@@ -96,28 +109,38 @@ class App extends React.Component {
         onDragStart={this.onDragStart}
         onDragEnd={this.onDragEnd}
       >
-        <Container>
-          {this.state.columnOrder.map((columnId, index) => {
-            const column = this.state.columns[columnId];
-            const tasks = column.taskIds.map(
-              taskId => this.state.tasks[taskId],
-            );
+        <Droppable
+          droppableId="all-columns"
+          direction="horizonal"
+          type="column"
+        >
+          {(provided) => (
+            <Container {...provided.droppableProps} ref={provided.innerRef}>
+              {this.state.columnOrder.map((columnId, index) => {
+                const column = this.state.columns[columnId];
+                const tasks = column.taskIds.map(
+                  (taskId) => this.state.tasks[taskId]
+                );
 
-            const isDropDisabled = index < this.state.homeIndex;
+                const isDropDisabled = index < this.state.homeIndex;
 
-            return (
-              <Column
-                key={column.id}
-                column={column}
-                tasks={tasks}
-                isDropDisabled={isDropDisabled}
-              />
-            );
-          })}
-        </Container>
+                return (
+                  <Column
+                    key={column.id}
+                    column={column}
+                    tasks={tasks}
+                    isDropDisabled={isDropDisabled}
+                    index={index}
+                  />
+                );
+              })}
+              {provided.placeholder}
+            </Container>
+          )}
+        </Droppable>
       </DragDropContext>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
